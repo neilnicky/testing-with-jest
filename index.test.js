3a3bf05e22ffaf229ec8a316f44c9219ed98c37b
Neil Nicholas <neilnicholaskanat@gmail.com>
2025-01-17 02:40:55 +0530

    feat: async/await

--- index.test.js
+++ index.test.js
@@ -35,10 +35,15 @@
 
 const fetchPromise = require("./index");
 
-test("the data is peanut butter", () => {
-  return expect(fetchPromise()).resolves.toBe("peanut butter");
-});
+// test("the data is peanut butter", () => {
+//   return expect(fetchPromise()).resolves.toBe("peanut butter");
+// });
 
-test("the fetch fails with an error", () => {
-  return expect(fetchPromise()).rejects.toThrow("error");
+// test("the fetch fails with an error", () => {
+//   return expect(fetchPromise()).rejects.toThrow("error");
+// });
+
+test("the data is peanut butter", async () => {
+  const data = await fetchPromise();
+  expect(data).toBe("peanut butter");
 });
